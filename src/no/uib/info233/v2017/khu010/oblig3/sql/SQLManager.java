1606cafe68b1ba57763f89a3f9a34d0685ecead2
ohstopityou <thomasovik@gmail.com>
2017-05-13 14:17:32 +0200

    sure bro

--- src/no/uib/info233/v2017/khu010/oblig3/sql/SQLManager.java
+++ src/no/uib/info233/v2017/khu010/oblig3/sql/SQLManager.java
@@ -1,6 +1,7 @@
 package no.uib.info233.v2017.khu010.oblig3.sql;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,6 +12,10 @@ import java.sql.Statement;
 
 import no.uib.info233.v2017.khu010.oblig3.game.MultiPlayerGame;
 
+import no.uib.info233.v2017.khu010.oblig3.players.Player;
+
+import java.sql.Statement;
+
 /**
  * Handles the SQL connection for updating
  * new scores in the game.
@@ -20,19 +25,101 @@ import no.uib.info233.v2017.khu010.oblig3.game.MultiPlayerGame;
  */
 public class SQLManager {
 	
-	private static SQLManager server = new SQLManager();
-	
-    private Connection con = null;
-    private PreparedStatement pst = null;
+    private static Connection con = null;
+    private static Statement stmt = null;
+    
+    private static SQLManager server = new SQLManager();
 
-    private String url = "jdbc:mysql://wildboy.uib.no/Khuna";
-    private String user = "Khuna";
-    private String password = "\"'mr{6)9m5wHfS3*";
+    //private String url = "jdbc:mysql://wildboy.uib.no/Khuna";
+    //private String user = "Khuna";
+    //private String password = "\"'mr{6)9m5wHfS3*";
     
-    private SQLManager() {}
+
     
-    public static SQLManager getConnection() {
-    	return server;
+	public static void main(String[] args) {
+		
+		try {
+			con = connect();
+		} catch (Exception e) {
+			System.out.println("connection failed");
+		}
+		server.hostGame(new Player("john"));
+			
+	}
+    
+    private SQLManager() { connect(); }
+    
+    private static Connection connect(){
+    	try {
+    		String url = "jdbc:mysql://localhost:8889/barinfo";
+            String user = "root";
+            String password = "root";
+        	
+            //create connection
+            con = DriverManager.getConnection(url, user, password);
+            
+            //create statement from connection
+            stmt = con.createStatement();
+            
+            //return connection
+            return con;
+            
+    	} catch (SQLException ex){
+    		System.out.println("connection failed");
+    		return null;
+    	}
+    	
+    }
+    
+    public boolean hostGame(Player player) { 
+    	try {
+    		String insertQuery = "INSERT INTO open_games " +
+    	"(player_1, player_1_random, player_2, player_2_random) VALUES (?, ?, ?, ?)";
+    		PreparedStatement pst = con.prepareStatement(insertQuery);
+    		
+    		//Set values
+    		pst.setString(1, player.getName());
+    		pst.setString(2, "random1234");
+    		pst.setString(3, null);
+    		pst.setString(4, null);
+    		
+    		//Execute update
+    		pst.executeUpdate();
+    		
+			return true;
+			
+    	} catch (Exception e){
+    		System.out.println("Could not connect because: " + e);
+    		
+    		return false;
+    	}
+	}
+    
+  	public ResultSet read() {
+
+    	try {
+    		
+    		PreparedStatement pst = con.prepareStatement("select * from open_games");
+    		ResultSet rs = pst.executeQuery();
+    		
+
+			while (rs.next()) {
+				System.out.println(rs.getString(2));
+				String player = rs.getString("player");
+				String score = rs.getString("score");
+
+				System.out.println("userid : " + player);
+				System.out.println("username : " + score);
+
+			}
+			return rs;
+    	} catch (Exception e){
+    		System.out.println("Could not connect because: " + e);
+    		return null;
+    	}
+    	
+    	//clear ranking table
+    	//stmt.executeUpdate("DELETE FROM ranking");
     }
 	
     /**
@@ -45,8 +132,10 @@ public class SQLManager {
 	public boolean addScore(String playerName, float playerScore){
 
         try {
-    		con = DriverManager.getConnection(url, user, password);
-            pst = con.prepareStatement("INSERT INTO `ranking`(`player`, `score`) VALUES (?, ?)");
+            //pst = con.prepareStatement("INSERT INTO `ranking`(`player`, `score`) VALUES (?, ?)");
+        	System.out.println(con);
+        	Statement stmt = con.createStatement();
+            System.out.println(rs);
             pst.setString(1, playerName);
             pst.setFloat(2, playerScore);
             pst.executeUpdate();
@@ -58,6 +147,7 @@ public class SQLManager {
         }
 	}
 	
+<<<<<<< Updated upstream
 	/**
 	 * Creates an entry for an open game in the database in which other players
 	 * can join.
@@ -115,6 +205,11 @@ public class SQLManager {
 		
 		return result;
 	}
+=======
+	public static boolean hostGame(String playerName) {
+		return false;
+    }
+>>>>>>> Stashed changes
 	
 	
 }
