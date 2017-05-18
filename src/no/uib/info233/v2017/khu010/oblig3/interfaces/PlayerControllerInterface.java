1ef5630119156400d81f9df1d542ec744935bd29
khuf <knuhuf@Knuts-MacBook-Pro-4.local>
2017-05-18 23:25:15 +0200

    Changed method signature in PlayerControllerInterface

--- src/no/uib/info233/v2017/khu010/oblig3/interfaces/PlayerControllerInterface.java
+++ src/no/uib/info233/v2017/khu010/oblig3/interfaces/PlayerControllerInterface.java
@@ -3,5 +3,5 @@ package no.uib.info233.v2017.khu010.oblig3.interfaces;
 public interface PlayerControllerInterface {
 	
 	//sends your move to the current game you´re playing
-	void sendMove(int move, String gameID);
+	void sendMove(int move);
 }
